ce970b6efac3773252a611bce04c34aae4d59a02
Ajith Reji <ajithvreji@gmail.com>
2024-01-21 22:08:13 +0530

    struct embedded

--- structs/main.go
+++ structs/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct{
+	email string
+	zipCode int
+}
+
 type person struct {
 	fname string
 	lname string
+	contact contactInfo
 	// age   int
 }
 
@@ -12,11 +18,29 @@ func main() {
 	var p1 person
 	p1.fname = "Ajith"
 	p1.lname = "Reji"
+	p1.contact.email="ajithvreji@github.com"
+	p1.contact.zipCode=685605
 	p1.printPerson()
+
+
+	hardpope := person{
+		fname: "hard",
+		lname:  "pope",
+		contact: contactInfo{
+			email: "hardpope@hp.com",
+			zipCode: 000011,
+		},
+	}
+
+	hardpope.printContact()
 
 
 }
 
 func (p person) printPerson(){
-	fmt.Printf("The person is %v %v", p.fname, p.lname)
+	fmt.Printf("The person is %v %v\n", p.fname, p.lname)
+}
+
+func (p person) printContact(){
+	fmt.Printf("The contact of %v is %+v\n", p.fname, p.contact)
 }
