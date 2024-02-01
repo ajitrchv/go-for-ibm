c8f82a6f7a7a153ee63b4c04e43f8ca09a490695
ajithvreji <ajithvreji@ibm.com>
2024-02-01 14:02:04 +0530

    lerning channels and go-routines part3

--- channels/main.go
+++ channels/main.go
@@ -21,22 +21,18 @@ func main() {
 	for _, link := range links {
 		go checkLink(link, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link, " might be down")
-		c <- "Might be down!"
+		out := fmt.Sprintf("%s might be down", link)
+		c <- out
 		return
 	}
-	fmt.Println(link, " is up!")
-	c <- "It is up!"
+	out := fmt.Sprintf("%s is up!!", link)
+	c <- out
 }
