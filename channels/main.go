7347b218f8a407a4b57cfa55f5269ecf8e0b7979
Ajith Reji <ajithvreji@gmail.com>
2024-02-01 20:54:09 +0530

    status checker code using channels

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,18 +22,19 @@ func main() {
 	for _, link := range links {
 		go checkLink(link, c)
 	}
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for{
+		time.Sleep(2 * time.Second)
+		go checkLink(<-c, c)
 	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
-		out := fmt.Sprintf("%s might be down", link)
-		c <- out
+		fmt.Println(link, " might be down!")
+		c <- link
 		return
 	}
-	out := fmt.Sprintf("%s is up!!", link)
-	c <- out
+	fmt.Println(link, " is up!!!")
+	c <- link
 }
